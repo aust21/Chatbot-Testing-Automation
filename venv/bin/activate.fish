6fee709935e3f213879b1107444ac6a852617d70
Austin Ngobeni <kngobeni223@gmail.com>
2025-02-11 11:37:14 +0200

    train model

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/khanyi_code/Chatbot-Testing-Automation/venv
+set -gx VIRTUAL_ENV /home/austin/projects/Chatbot-Testing-Automation/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
